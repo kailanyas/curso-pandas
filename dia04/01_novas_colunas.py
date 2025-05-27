918f36c084ed2e28ff0c36d9c18fa3556c918697
kailany <kailanyalves04@gmail.com>
2025-05-27 19:17:43 -0300

    aplicando operacoes em colunas com numpy

--- dia04/01_novas_colunas.py
+++ dia04/01_novas_colunas.py
@@ -1,12 +1,13 @@
 # %%
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv("../data/clientes.csv")
 df.head()
 
 # %%
 
-#operação aritmética em uma coluna -> criando nova coluna
+#operação com escalar em uma coluna -> criando nova coluna
 df["pontos_100"] = df["qtdePontos"] + 100
 df
 
@@ -28,5 +29,30 @@ df.head()
 df["qtdeSocial"] = df["flEmail"] + df["flTwitch"] + df["flYouTube"] + df["flBlueSky"] + df["flInstagram"]
 df.head()
 
+df["todasSocial"] = df["flEmail"] * df["flTwitch"] * df["flYouTube"] * df["flBlueSky"] * df["flInstagram"]
+df.head()
 
+# %%
+
+#descreve a coluna -> media, desvio padrao, etc
+df["qtdePontos"].describe()
+
+# %%
+
+# aplicando log na coluna -> usa numpy (np é bom pra aplicar funcoes matematicas)
+df["logPontos"] = np.log(df["qtdePontos"] + 1) #+1 evita log0 ; log deixa a distribuição MENOS assimetrica
+
+# %%
+
+# mostrando o efeito do log em graficos
+import matplotlib.pyplot as plt
+
+graficoPontos = plt.hist(df["qtdePontos"])
+plt.grid(True)
+plt.show()
+
+#parece mais com uma distribuiçao normal
+plt.hist(df["logPontos"])
+plt.grid(True)
+plt.show()
 
